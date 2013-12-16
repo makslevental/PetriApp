648d43575f3bd353ffd9f1030245763e178ed46d
max <maksim.levental@gmail.com>
2013-12-16 14:24:16 -0500

    first commit

---
+++ myapp/flaskr/app/__init__.py
@@ -0,0 +1,50 @@
+import os
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from flask.ext.mail import Mail
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
+
+
+
+
+
+app = Flask(__name__, static_folder='../../static', template_folder='../../templates')
+
+app.config.from_object('config')
+db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
+
+mail = Mail(app)
+
+
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler, RotatingFileHandler
+
+    file_handler = RotatingFileHandler(os.path.join(basedir, 'tmp/microblog.log'), 'a', 1 * 1024 * 1024, 10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('microblog startup')
+
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_PASSWORD)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure',
+                               credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
+
+from app import views, models
+
+from momentjs import momentjs
+
+app.jinja_env.globals['momentjs'] = momentjs
+
---
+++ myapp/flaskr/run.py
@@ -0,0 +1,10 @@
+__author__ = 'max'
+
+
+
+import sys
+sys.path.insert(0,'/var/www/flaskr')
+from app import app
+application = app 
+#app.run(debug=True)
+
