4e42fab351d1c952e9987916dfb5f70af0e58abf
max <maksim.levental@gmail.com>
2013-12-17 21:39:32 -0500

    mysecondapp

--- mysecondapp/petri/app/views.py
+++ mysecondapp/petri/app/views.py
@@ -1,4 +1,4 @@
-from ubuntuone.storageprotocol import request
+
 __author__ = 'max'
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
