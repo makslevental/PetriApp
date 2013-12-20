8a9fe69021f14f224808f392fd4913e6905dbed9
max <maksim.levental@gmail.com>
2013-12-20 17:25:02 -0500

    made much prettier

--- mysecondapp/petri/app/__init__.py
+++ mysecondapp/petri/app/__init__.py
@@ -2,8 +2,7 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
-from flask.ext.openid import OpenID
-from flask.ext.mail import Mail
+from flask.ext.mail import Mail, Message
 from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 
@@ -18,10 +17,9 @@ db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
-oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 mail = Mail(app)
-
+mail.init_app(app)
 
 if not app.debug:
     import logging
